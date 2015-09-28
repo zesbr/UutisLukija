6d110a7aefa1c7c2275066dbe825bf904e293bad
zesbr <jesse_ruuth@hotmail.com>
2015-09-28 20:48:50 +0300

    Heroku config

--- src/main/java/com/mycompany/uutislukija/App.java
+++ src/main/java/com/mycompany/uutislukija/App.java
@@ -13,4 +13,12 @@ public class App {
             System.out.println("*ERROR*");
         }
     }
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
