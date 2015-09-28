2c2c8dd22be4a94b3652546fcb895aacc7a176eb
zesbr <jesse_ruuth@hotmail.com>
2015-09-28 20:54:27 +0300

    Heroku config

--- src/main/java/com/mycompany/uutislukija/App.java
+++ src/main/java/com/mycompany/uutislukija/App.java
@@ -4,10 +4,12 @@ import com.mycompany.hackernewsuutiset.HackerPaivanUutiset;
 import com.mycompany.uutislukija.config.WebConfig;
 import com.mycompany.uutislukija.service.HackerNews;
 import java.io.IOException;
+import static spark.SparkBase.port;
 
 public class App {
     public static void main(String[] args){
         try {
+            port(getHerokuAssignedPort());
             new WebConfig(new HackerNews(new HackerPaivanUutiset()));
         } catch (IOException exception) {
             System.out.println("*ERROR*");
